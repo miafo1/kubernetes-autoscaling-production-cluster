cdddf935872555b58ff5a6d191fb20868832523d
miafo1 <miafoarnauld@gmail.com>
2025-12-31 02:22:01 -0800

    fix: make ecr cleanup script dynamic

--- scripts/cleanup-ecr.sh
+++ scripts/cleanup-ecr.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
 # cleanup-ecr.sh
 # Delete all images from ECR repository before destroying infrastructure
 
-REGION="us-east-1"
-REPO_NAME="k3s-demo-cluster-app"
+# Fetch details from Terraform
+REPO_URL=$(cd infra && terraform output -raw ecr_repository_url)
+REGION=$(echo $REPO_URL | cut -d'.' -f4)
+REPO_NAME=$(echo $REPO_URL | cut -d'/' -f2)
 
-echo "Deleting all images from ECR repository: $REPO_NAME"
+echo "Deleting all images from ECR repository: $REPO_NAME in region: $REGION"
 
 # Get all image digests
 IMAGE_DIGESTS=$(aws ecr list-images --repository-name $REPO_NAME --region $REGION --query 'imageIds[*].imageDigest' --output text)
