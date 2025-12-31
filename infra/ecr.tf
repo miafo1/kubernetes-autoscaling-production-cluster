1a834710b510cc55eb10bc2ac85ceaf6d1647121
miafo1 <miafoarnauld@gmail.com>
2025-12-31 01:24:09 -0800

    fix: enable ECR force_delete for cleanup

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -4,6 +4,7 @@
 resource "aws_ecr_repository" "app_repo" {
   name                 = "${var.cluster_name}-app"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true  # Allow deletion even if images exist
 
   image_scanning_configuration {
     scan_on_push = true
